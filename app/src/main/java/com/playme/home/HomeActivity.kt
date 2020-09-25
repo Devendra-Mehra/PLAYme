349428ca5790bf28d3ca769b559a6c2da8e37f67
Devendra Mehra <mehradevendra4@gmail.com>
2020-09-25 17:54:04 +0530

    app style changed and application class added

--- app/src/main/java/com/playme/home/HomeActivity.kt
+++ app/src/main/java/com/playme/home/HomeActivity.kt
@@ -1,7 +1,8 @@
-package com.playme
+package com.playme.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.playme.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
